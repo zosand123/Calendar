4a31eddffddd7bd009ce8520d5e0f42639ca9cb5
suyeon <suyeon@172.30.1.24>
2020-11-12 23:28:37 +0900

    Method 추가

--- src/oop/Method.java
+++ src/oop/Method.java
@@ -1,11 +1,12 @@
 package oop;
 
 public class Method {
-
+//hihi
 	public static void main(String[] args) {
 		
 		int result = Method2.max(2,8);
 		System.out.println(result);
+	System.out.println("sss");
 	}
 	
 	
